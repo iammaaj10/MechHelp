2ef015ea6f55e61828adff5179fe4c64e4a81561
Maaj <maajb1122@gmail.com>
2024-10-12 20:01:54 +0530

    committed

--- Client/mechhelp/src/App.jsx
+++ Client/mechhelp/src/App.jsx
@@ -7,7 +7,13 @@ function App() {
 
   return (
     <>
-     <h1 className='text-xl text-gray-700 text-center '>Allah is great </h1>
+    <div className='h-screen w-full bg-black justify-center items-center flex'>
+      <div className=''>
+      <h1 className='text-5xl text-blue-400 text-center font-bold'>MEchHelp</h1>
+      <h1 className='text-2xl text-gray-700 text-center font-semibold'>Coming Soon </h1>
+      </div>
+   
+     </div>
     </>
   )
 }
